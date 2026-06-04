df354e80f11193a07891089b5ea5bf4465a70196
glermFF <guilherme.ff@aluno.ufop.edu.br>
2026-06-03 21:35:26 -0300

    container: adiciona separação de serviços para BD, Back e Front no dockercompose

--- application/app-backend/src/server.ts
+++ application/app-backend/src/server.ts
@@ -7,12 +7,10 @@ dotenv.config();
 const PORT = process.env.PORT || 3000;
 
 const iniciarServidor = async () => {
-  console.log('⏳ Inicializando sistemas...');
+  console.log('Inicializando sistemas...');
 
-  // 1. Garante que o banco de grafos está ativo antes de aceitar requisições
   await verificarConexaoNeo4j();
 
-  // 2. Levanta o servidor Express na porta desejada
   app.listen(PORT, () => {
     console.log(`Servidor Express rodando na porta http://localhost:${PORT}`);
     console.log(`Rota de teste disponível em: http://localhost:${PORT}/health`);
